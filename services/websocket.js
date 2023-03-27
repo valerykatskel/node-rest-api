21a34ba5b6be98b40cae6d6cc5bb73924a5bf3a4
Valery Katskel <ne.vse.srazu@gmail.com>
2023-03-27 23:56:06 +0300

    websocket client connect updated

--- services/websocket.js
+++ services/websocket.js
@@ -2,22 +2,24 @@ const WebSocket = require('ws');
 
 function createWebSocketServer() {
   // Create a WebSocket server
-  const wsServer = new WebSocket.Server({ port: 8080 });
-
+  const port = process.env.WS_PORT || 8080;
+  const wsServer = new WebSocket.Server({ port });
+  wsServer.on('listening', () => {
+    console.log(`WebSocket server started on http://localhost:${port}`);
+  });
   // Keep track of connected clients
   const clients = new Set();
 
   // Handle WebSocket connections
   wsServer.on('connection', (socket) => {
-    //console.log(socket)
-    console.log(`WebSocket client connected`);
+    console.log(`WebSocket клиент подключился`);
 
     // Add client to set
     clients.add(socket);
 
     // Handle WebSocket messages
     socket.on('message', (data) => {
-      console.log(`Received message: ${data} from client`);
+      console.log(`Получено сообщение: ${data} from client`);
 
       // Broadcast message to other clients
       for (const client of clients) {
@@ -29,7 +31,7 @@ function createWebSocketServer() {
 
     // Handle WebSocket disconnections
     socket.on('close', () => {
-      console.log('WebSocket client disconnected');
+      console.log('WebSocket клиент отключился');
 
       // Remove client from set
       clients.delete(socket);
