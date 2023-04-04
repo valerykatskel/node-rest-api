a996fc8e48c5dc4d3e8d7b1ccd4abbc153d00b4e
Valery Katskel <ne.vse.srazu@gmail.com>
2023-04-05 01:53:38 +0300

    fix

--- services/wssHandlers.js
+++ services/wssHandlers.js
@@ -2,14 +2,27 @@ require('dotenv').config();
 const clients = new Set();
 
 const onCloseHandler = (socket, clients) => {
-    console.log(`WebSocket disconnected. Total: ${clients.size}`);
     clients.delete(socket);
+    console.log(`WebSocket disconnected. Total: ${clients.size}`);
 }
 
 const onConnectionHandler = (socket) => {
+
+    if (!clients.has(socket)) {
+        clients.add(socket);
+        console.log(`Added a new client. Total: ${clients.size}`);
+    }
+
     socket.on('message', (data) => {
         const mes = data.toString('utf8');
-        if (clients.has(socket)) {
+
+        for (const client of clients) {
+            if (client !== socket) {
+                client.send(`${mes}`);
+            }
+        }
+
+        /*if (clients.has(socket)) {
             // Broadcast message to other clients
             for (const client of clients) {
                 if (client !== socket) {
@@ -17,26 +30,26 @@ const onConnectionHandler = (socket) => {
                 }
             }
         } else {
-            try {
+            //try {
                 //const { login, machineId, message } = JSON.parse(data);
 
                 // Validate the login and machine ID
-                /*if (machineId !== process.env.ALLOW_MACHINEID) {
+                if (machineId !== process.env.ALLOW_MACHINEID) {
                     console.log(`Rejected client with invalid credentials`);
                     socket.close(1007, 'Invalid data');
                     return;
-                }*/
+                }
                 
                 // Add client to set
                 
-                clients.add(socket);
-                console.log(`Added a new client. Total: ${clients.size}`);
+                //clients.add(socket);
+                //console.log(`Added a new client. Total: ${clients.size}`);
             } catch (error) {
                 console.log(`Rejected client with wrong message format`);
                 socket.close(1003, 'Unsupported data');
                 return;
             }
-        }
+        }*/
     });
     socket.on('close', () => onCloseHandler(socket, clients));
 }
