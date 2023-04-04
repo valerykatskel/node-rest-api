4afc5b015b21247c32481b473ae1d8158f7d3274
Valery Katskel <ne.vse.srazu@gmail.com>
2023-04-05 00:21:33 +0300

    final fixes

--- services/wssHandlers.js
+++ services/wssHandlers.js
@@ -6,34 +6,37 @@ const onCloseHandler = (data) => console.log(`WebSocket disconnected, code is ${
 const onConnectionHandler = (socket) => {
     socket.on('message', (data) => {
         const mes = data.toString('utf8');
-        if (mes.indexOf('{') === -1 || mes.indexOf('{') > 0) {
-            console.log(`Rejected client with wrong message format`);
-            socket.close(1003, 'Unsupported data');
-            return;
-        }
-        try {
-            const { login, machineId, message } = JSON.parse(data);
-            // Validate the login and machine ID
-            if (login !== process.env.handlarVXv2 || machineId !== process.env.ALLOW_MACHINEID) {
-                console.log(`Rejected client with invalid credentials: ${socket.remoteAddress}`);
-                socket.close(1007, 'Invalid data');
-                return;
-            }
-            
-            console.log(`Accepted client with login=${login} and machineId=${machineId}: ${socket.remoteAddress}. Total: ${clients.size}`);
-
-            // Add client to set
-            clients.add(socket);
+        if (clients.has(socket)) {
             // Broadcast message to other clients
             for (const client of clients) {
                 if (client !== socket) {
-                    client.send(`Переслано всем: ${data}`);
+                    client.send(`Переслано всем: ${mes}`);
                 }
             }
-        } catch (error) {
-            console.log(`Rejected client with wrong message format`);
-            socket.close(1003, 'Unsupported data');
-            return;
+        } else {
+            if (mes.indexOf('{') === -1 || mes.indexOf('{') > 0) {
+                console.log(`Rejected client with wrong message format`);
+                socket.close(1003, 'Unsupported data');
+                return;
+            }
+            try {
+                const { login, machineId, message } = JSON.parse(data);
+                // Validate the login and machine ID
+                if (login !== process.env.handlarVXv2 || machineId !== process.env.ALLOW_MACHINEID) {
+                    console.log(`Rejected client with invalid credentials: ${socket.remoteAddress}`);
+                    socket.close(1007, 'Invalid data');
+                    return;
+                }
+                
+                console.log(`Accepted client with login=${login} and machineId=${machineId}: ${socket.remoteAddress}. Total: ${clients.size}`);
+    
+                // Add client to set
+                clients.add(socket);
+            } catch (error) {
+                console.log(`Rejected client with wrong message format`);
+                socket.close(1003, 'Unsupported data');
+                return;
+            }
         }
     });
     socket.on('close', onCloseHandler);
