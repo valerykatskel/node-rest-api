ca914451fd120245c0e4e5681c1f25271cdfe471
Valery Katskel <ne.vse.srazu@gmail.com>
2023-04-05 00:44:22 +0300

    small refactoring

--- services/wssHandlers.js
+++ services/wssHandlers.js
@@ -14,23 +14,18 @@ const onConnectionHandler = (socket) => {
                 }
             }
         } else {
-            if (mes.indexOf('{') === -1 || mes.indexOf('{') > 0) {
-                console.log(`Rejected client with wrong message format`);
-                socket.close(1003, 'Unsupported data');
-                return;
-            }
             try {
                 const { login, machineId, message } = JSON.parse(data);
-                console.log(login);
-                console.log(machineId)
+
                 // Validate the login and machine ID
-                if (login !== process.env.ALLOW_LOGIN || machineId !== process.env.ALLOW_MACHINEID) {
-                    console.log(`Rejected client with invalid credentials: ${socket.remoteAddress}`);
+                if (machineId !== process.env.ALLOW_MACHINEID) {
+                    console.log(`Rejected client with invalid credentials`);
                     socket.close(1007, 'Invalid data');
                     return;
                 }
                 
                 // Add client to set
+                
                 clients.add(socket);
                 console.log(`Accepted client with login=${login} and machineId=${machineId}: ${socket.remoteAddress}. Total: ${clients.size}`);
             } catch (error) {
