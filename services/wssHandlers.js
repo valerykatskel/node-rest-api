1908562701149cab383bc861d47edb35b3b8e088
Valery Katskel <ne.vse.srazu@gmail.com>
2023-04-04 23:30:15 +0300

    add check client by login and machineid

--- services/wssHandlers.js
+++ services/wssHandlers.js
@@ -3,20 +3,31 @@ const clients = new Set();
 const onCloseHandler = () => console.log('WebSocket disconnected');
 
 const onConnectionHandler = (socket) => {
-    // Add client to set
-    clients.add(socket);
-    console.log(`WebSocket клиент подключился c IP ${socket.remoteAddress}.  Всего клиентов: ${clients.size}`);
-
     socket.on('message', (data) => {
-        console.log(`Получено сообщение: ${data} from client`);
-        // Broadcast message to other clients
-        for (const client of clients) {
-            if (client !== socket) {
-                client.send(`Переслано всем: ${data}`);
-            }
+      // Parse the incoming message
+      const { login, machineId, message } = JSON.parse(data);
+  
+      // Validate the login and machine ID
+      if (login !== process.env.ALLOW_LOGIN || machineId !== process.env.ALLOW_MACHINEID) {
+        console.log(`Rejected client with invalid credentials: ${socket.remoteAddress}`);
+        socket.close();
+        return;
+      }
+  
+      console.log(`Accepted client with login=${login} and machineId=${machineId}: ${socket.remoteAddress}`);
+  
+      // Add client to set
+      clients.add(socket);
+  
+      // Broadcast message to other clients
+      for (const client of clients) {
+        if (client !== socket) {
+          client.send(`Переслано всем: ${message}`);
         }
+      }
     });
+  
     socket.on('close', onCloseHandler);
-}
+  };
 
 module.exports = { onConnectionHandler };
